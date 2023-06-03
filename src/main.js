fc1e7633b618617c895ed87c6df7d1600e6a3adc
David1010 <jorge.beltran1.mt@usa.edu.co>
2023-06-03 18:20:43 -0500

    "correccion"

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import routerInstance from './router/indexRouter';
-import store from './store/indexStore';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap';
+import { createApp } from "vue";
+import App from "./App.vue";
+import routerInstance from "./router/indexRouter";
+import store from "./store/indexStore";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap";
 
 const app = createApp(App);
 app.use(routerInstance);
 app.use(store);
-app.mount('#app');
+app.mount("#app");
